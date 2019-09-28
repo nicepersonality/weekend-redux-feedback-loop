efe7fba62d10f10e0fd04368097017c2dd591856
Lilith Bentley <sbentley@gmail.com>
2019-09-28 17:56:30 -0500

    Add welcome message and comment feedback page.

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import InputTextarea from '../Elements/InputTextarea';
+import NextButton from '../Elements/NextButton';
 
 class Comments extends Component {
   render() {
     return (
       <section className="Comments-component">
-        <h2>Comments</h2>
+        <h2>Feedback Comments</h2>
+        <p>Do you have any other <strong>comments</strong> for us?</p>
+        <div><label>My comments:
+          <InputTextarea 
+            feedbackType="comments" 
+            placeholder="(Optional)"
+          />
+        </label></div>
+        <NextButton
+          required="false"
+          history={this.props.history}
+          nextPage="/review"
+        />
       </section>
     );
   }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import NextButton from '../Elements/NextButton';
 
 class Home extends Component {
   render() {
     return (
       <section className="Home-component">
-        <h2>Home</h2>
+        <h2>Welcome!</h2>
+        <p>Your feedback is <strong>extremely h*ckin’ important</strong> to us, and Luke will be sad if you don’t provide it. Don’t make Luke sad!</p>
+        <NextButton
+          required="false"
+          history={this.props.history}
+          nextPage="/feeling"
+        />
       </section>
     );
   }
