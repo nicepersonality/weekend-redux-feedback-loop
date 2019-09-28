0344f8fa2af204f412833433ef134cd443baae30
Lilith Bentley <sbentley@gmail.com>
2019-09-28 15:13:09 -0500

    Add redux store and feedback reducer.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {connect} from 'react-redux';
+
 import './App.css';
 
 class App extends Component {
@@ -8,12 +10,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
+          <h2>Don't forget it!</h2>
         </header>
-        <br/>
       </div>
     );
   }
 }
 
-export default App;
+const putReduxStateOnProps = (reduxState) => ({
+  reduxState
+}) // put the redux store values in component props
+export default connect (putReduxStateOnProps) (App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import './index.css';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const feedbackReducer = (state = {}, action) => {
+  // Add to the order in the store
+  // Sample feedback state:
+  // {
+  //   feeling: 4,
+  //   understanding: 4,
+  //   support: 5,
+  //   comments: 'Doing great1'
+  // }
+  if (action.type === 'SET_FEEDBACK') {
+    return {...state, ...action.payload};
+  } // end if
+  return state;
+} // end feedbackReducer
+
+const reduxStore = createStore(
+  // using combineReducers in case more are needed later
+  combineReducers({
+    feedbackReducer
+  }),
+  // log reduxStore details to console
+  applyMiddleware(logger)
+); // end reduxStore
+
+ReactDOM.render(
+  // make the store available to the entire app
+  <Provider store={reduxStore}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+); // end render
+
+// registerServiceWorker();
